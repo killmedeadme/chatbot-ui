6376bdaf7878d39f6d5e7c7fbe885bbb7fefaf25
killmedeadme <killmedeadme@gmail.com>
2025-07-03 03:13:49 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,31 +2,29 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true"
 })
 
-const withPWA = require("next-pwa")({
-  dest: "public"
-})
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost"
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1"
+      },
+      {
+        protocol: "https",
+        hostname: "**"
+      }
+    ]
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["sharp", "onnxruntime-node"]
+  }
+}
 
-module.exports = withBundleAnalyzer(
-  withPWA({
-    reactStrictMode: true,
-    images: {
-      remotePatterns: [
-        {
-          protocol: "http",
-          hostname: "localhost"
-        },
-        {
-          protocol: "http",
-          hostname: "127.0.0.1"
-        },
-        {
-          protocol: "https",
-          hostname: "**"
-        }
-      ]
-    },
-    experimental: {
-      serverComponentsExternalPackages: ["sharp", "onnxruntime-node"]
-    }
-  })
-)
+// ✅ PWA機能は完全に外す → `withPWA` を消す
+module.exports = withBundleAnalyzer(nextConfig)
