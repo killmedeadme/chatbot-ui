019be61b57ed0841fa0f1451861fd8a1d9fd4e1d
killmedeadme <killmedeadme@gmail.com>
2025-07-03 03:17:53 +0900

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,37 @@
+import { NextRequest, NextResponse } from 'next/server'
+
 export function middleware(req: NextRequest) {
   const pathname = req.nextUrl.pathname
-  console.log('🔥 pathname:', pathname)
 
+  // 除外パス
   if (
     pathname.startsWith('/_next') ||
     pathname.startsWith('/api') ||
-    pathname.startsWith('/favicon.ico') ||
-    pathname.includes('manifest') ||
-    pathname.startsWith('/robots.txt') ||
-    pathname.startsWith('/sitemap.xml')
+    pathname.startsWith('/favicon.ico')
   ) {
-    console.log('✅ 除外ヒット:', pathname)
     return NextResponse.next()
   }
 
-  return NextResponse.next()
+  const basicAuth = req.headers.get('authorization')
+
+  const USER = process.env.BASIC_AUTH_USER || ''
+  const PASS = process.env.BASIC_AUTH_PASS || ''
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1]
+    const [user, pass] = atob(authValue).split(':')
+
+    if (user === USER && pass === PASS) {
+      return NextResponse.next()
+    }
+  }
+
+  return new Response('Unauthorized', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  })
 }
 
 export const config = {
