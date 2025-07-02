16aaccdd17636a024b1b4dc5c9f69f566388713b
killmedeadme <killmedeadme@gmail.com>
2025-07-02 20:58:46 +0900

    Update page.tsx
    構文変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 // app/page.tsx
 import { redirect } from 'next/navigation'
 
-export default function RootPage() {
-  redirect('/ja')
+export default function RootPage(): JSX.Element {
+  return redirect('/ja')
 }
