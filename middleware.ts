3eae3c57beb70e49dbf2c9452a19c15a58c3c5f7
killmedeadme <killmedeadme@gmail.com>
2025-07-02 18:50:44 +0900

    middleware.ts を更新

--- middleware.ts
+++ middleware.ts
@@ -1,46 +1,24 @@
-import { createClient } from "@/lib/supabase/middleware"
-import { i18nRouter } from "next-i18n-router"
-import { NextResponse, type NextRequest } from "next/server"
-import i18nConfig from "./i18nConfig"
+import { NextResponse } from 'next/server'
 
-export async function middleware(request: NextRequest) {
-  const i18nResult = i18nRouter(request, i18nConfig)
-  if (i18nResult) return i18nResult
+export function middleware(req) {
+  const basicAuth = req.headers.get('authorization')
 
-  try {
-    const { supabase, response } = createClient(request)
+  const USER = process.env.BASIC_AUTH_USER || ''
+  const PASS = process.env.BASIC_AUTH_PASS || ''
 
-    const session = await supabase.auth.getSession()
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1]
+    const [user, pass] = atob(authValue).split(':')
 
-    const redirectToChat = session && request.nextUrl.pathname === "/"
-
-    if (redirectToChat) {
-      const { data: homeWorkspace, error } = await supabase
-        .from("workspaces")
-        .select("*")
-        .eq("user_id", session.data.session?.user.id)
-        .eq("is_home", true)
-        .single()
-
-      if (!homeWorkspace) {
-        throw new Error(error?.message)
-      }
-
-      return NextResponse.redirect(
-        new URL(`/${homeWorkspace.id}/chat`, request.url)
-      )
+    if (user === USER && pass === PASS) {
+      return NextResponse.next()
     }
-
-    return response
-  } catch (e) {
-    return NextResponse.next({
-      request: {
-        headers: request.headers
-      }
-    })
   }
-}
 
-export const config = {
-  matcher: "/((?!api|static|.*\\..*|_next|auth).*)"
+  return new Response('Unauthorized', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  })
 }
